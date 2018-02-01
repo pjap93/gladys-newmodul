19744d71f06ab48db2ae6ecc696292f3086681bb
pjap93 <pjap9392@gmail.com>
2018-02-01 22:39:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function(sails) {
+    
+    //var setup = require('./lib/setup.js');
+    //var exec = require('./lib/exec.js');
+    //var init = require('./lib/init.js');
+    //var install = require('./lib/install.js');
+    //var uninstall = require('./lib/uninstall.js');
+
+    //gladys.on('ready', function(){
+    //    init();
+    //});
+
+    return {
+          //setup: setup,
+          //install: install,
+          //uninstall: uninstall,
+	        //exec: exec
+        };
+};
